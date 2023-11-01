a8083b7aab0ecf98dd270a776f515892d10f468a
Pedro Herrera <pedroherlop@hotmail.com>
2023-11-01 20:54:31 +0100

    refactor: Rename variable to represent route instead of endpoint

--- src/routes/healthcheck.ts
+++ src/routes/healthcheck.ts
@@ -1,15 +1,15 @@
 import { type Express, text } from 'express';
 
-const HEALTHCHECK_ENDPOINT = '/healthcheck';
+const HEALTHCHECK_ROUTE = '/healthcheck';
 
 export default function healthcheck(express: Express) {
-	express.get(HEALTHCHECK_ENDPOINT, (request, response) => {
+	express.get(HEALTHCHECK_ROUTE, (request, response) => {
 		response
 			.status(200)
 			.send('Server up and running');
 	});
 
-	express.post(HEALTHCHECK_ENDPOINT, text(), (request, response) => {
+	express.post(HEALTHCHECK_ROUTE, text(), (request, response) => {
 		response
 			.status(200)
 			.send('Server up and running, Mr/Ms: ' + request.body);
